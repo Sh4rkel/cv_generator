73fa6510484e599bc85064d9a9388e69540431db
Sh4rkel <filipdaniel2000@yahoo.com>
2024-06-05 09:04:01 +0300

    finish2

--- Pages/index.php
+++ Pages/index.php
@@ -26,28 +26,6 @@ include '../Scripts/cv_db.php';
 <main>
     <h2>CV Generator</h2>
     <p>Creaza-ti propriul CV.</p>
-    <?php if (isset($_SESSION['username'])): ?>
-        <h3>Your CVs</h3>
-        <?php
-        $username = $_SESSION['username'];
-        $result = $conn->query("SELECT * FROM cvs WHERE username = '$username' ORDER BY created_at DESC");
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo '<p>';
-                echo 'Name: <a href="../cvs/' . $row['name'] . '.pdf">' . $row['name'] . '</a><br>';
-                echo 'Contact: ' . $row['contact'] . '<br>';
-                echo 'Objective: ' . $row['objective'] . '<br>';
-                echo 'Education: ' . $row['education'] . '<br>';
-                echo 'Skills: ' . implode(', ', $row['skills']) . '<br>'; // Convert the skills array to a string
-                echo 'Experience: ' . $row['experience'] . '<br>';
-                echo 'References: ' . $row['references'] . '<br>';
-                echo '</p>';
-            }
-        } else {
-            echo "No CVs found.";
-        }
-        ?>
-    <?php endif; ?>
 </main>
 <footer>
     <p>&copy; 2024 CV Generator. All rights reserved.</p>
