3183da25f05995efff4f152e029cfa0e71cc023d
Daniel-Marian Filip <99269936+Sh4rkel@users.noreply.github.com>
2024-09-01 02:29:57 +0300

    Update login.php

--- Scripts/login.php
+++ Scripts/login.php
@@ -1,13 +1,10 @@
 <?php
 include 'db.php';
 
-// Assuming $conn is your mysqli connection
 $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
 
-// Bind the parameter to the SQL query. 's' indicates that the parameter is a string.
 $stmt->bind_param('s', $username);
 
-// Assuming $username is the value you want to check
 $username = $_POST['username'];
 
 $stmt->execute();
@@ -17,7 +14,7 @@ if ($result->num_rows > 0) {
     header('Location: ../Pages/index.php');
     exit;
 } else {
-    // User does not exist
+    exit;
 }
 
 $stmt->close();
